e34693379bfee3e3762d6397a6162bb9cb44658b
Brian <gamerfunky78@gmail.com>
2024-12-12 18:28:28 +0300

    update click functionality.

--- index.js
+++ index.js
@@ -9,80 +9,118 @@ const pic3Desc = document.getElementById('pic3-desc')
 
 
 
-async function getNews(){
+async function getNews() {
 
 
-    const url = 'https://cr-news-api-service.prd.crunchyrollsvc.com/v1/en-US/stories/search?category=Announcements,News&page_size=20&page=5'
-    const corsProxyUrl = 'https://corsproxy.io/?'
-    
-
-    const response = await fetch( corsProxyUrl + url )
-    const data = await response.json();
-
-    console.log(data)
-    
-    data.stories.forEach(newsItem => {
-
-        const newsCard = document.createElement('div')
-        const picture = document.createElement('img')
-        const desc = document.createElement('p')
-
-        newsCard.classList.add('news-card')
-        
-
-        const mainPic = newsItem.content.thumbnail.filename
-        const mainDesc = newsItem.content.headline
-
-        
-        picture.src = mainPic
-        desc.textContent = mainDesc
-
-        picture.style.width = '100%'
-
-        picture.classList.add('news-image')
-        desc.classList.add('descriptions2')
+  const url = 'https://cr-news-api-service.prd.crunchyrollsvc.com/v1/en-US/stories/search?category=Announcements,News&page_size=20&page=5'
+  const corsProxyUrl = 'https://corsproxy.io/?'
 
 
-        newsCard.appendChild(picture)
-        newsCard.appendChild(desc)
+  const response = await fetch(corsProxyUrl + url)
+  const data = await response.json();
+  const link1 = document.createElement('a')
+  const link2 = document.createElement('a')
 
-        newsContainer.appendChild(newsCard)
 
-    });
 
-    
-    
+  linkForPic1 = data.stories[11].slug
+  linkForPic2 = data.stories[19].slug
+
+  link1.href = `https://www.google.com/search?q=${encodeURIComponent(linkForPic1)}`;
+  link2.href = `https://www.google.com/search?q=${encodeURIComponent(linkForPic2)}`;
+  link1.target = '_blank'
+  link2.target = '_blank'
+  console.log(data)
+
+  function linkToNewTab(url) {
+    window.open(url, '_blank');  // Opens the link in a new tab
+  }
+
+  pic2.src = data.stories[11].content.thumbnail.filename
+  pic3.src = data.stories[19].content.thumbnail.filename
+
+  pic2Desc.textContent = data.stories[1].content.headline
+  pic3Desc.textContent = data.stories[2].content.headline
+
+  pic2.classList.add('news-image2')
+  pic3.classList.add('news-image2')
+  pic2Desc.classList.add('descriptions2')
+  pic3Desc.classList.add('descriptions2')
+
+  pic2.onclick = () => {
+    linkToNewTab(link1)
+  }
+  pic3.onclick = () => {
+    linkToNewTab(link2)
+  }
+
+  data.stories.forEach(newsItem => {
+
+    const newsCard = document.createElement('div')
+    const picture = document.createElement('img')
+    const desc = document.createElement('p')
+
+    newsCard.classList.add('news-card')
+
+
+    const mainPic = newsItem.content.thumbnail.filename
+    const mainDesc = newsItem.content.headline
+    const link = newsItem.slug
+    const mainLink = document.createElement('a')
+
+    mainLink.href = mainLink.href = `https://www.google.com/search?q=${encodeURIComponent(link)}`;
+
+    picture.src = mainPic
+    desc.textContent = mainDesc
+
+    picture.style.width = '100%'
+
+    picture.classList.add('news-image')
+    desc.classList.add('descriptions2')
+    mainLink.target = '_blank'
+
+    newsCard.appendChild(picture)
+    newsCard.appendChild(desc)
+    newsContainer.appendChild(mainLink)
+    mainLink.appendChild(newsCard)
+
+  });
+
+
+
 
 
 }
 
-async function getStory(){
+async function getStory() {
 
 
   const url = 'https://cr-news-api-service.prd.crunchyrollsvc.com/v1/en-US/stories/search?category=Announcements,News&page_size=1&page=5'
   const corsProxyUrl = 'https://corsproxy.io/?'
 
-
-  const response = await fetch( corsProxyUrl + url )
+  const response = await fetch(corsProxyUrl + url)
   const data = await response.json();
+  const link = document.createElement('a')
+  linkForPic1 = data.stories[0].slug
+  link.href = `https://www.google.com/search?q=${encodeURIComponent(linkForPic1)}`;
 
-
-  console.log(data)
 
   const mainPic = data.stories[0].content.thumbnail.filename
   const mainDesc = data.stories[0].content.headline
   mainPicture.classList.add('mainpicture')
   mainPicture.src = mainPic
   Descs.textContent = mainDesc
-  pic2.src = data.stories[1].content.thumbnail.filename
-  pic3.src = data.stories[2].content.thumbnail.filename
-  pic2Desc.textContent = data.stories[1].content.headline
-  pic3Desc.textContent = data.stories[2].content.headline
-  pic2.classList.add('news-image2')
-  pic3.classList.add('news-image2')
-  pic2Desc.classList.add('descriptions2')
-  pic3Desc.classList.add('descriptions2')
+
+  function linkToNewTab(url) {
+    window.open(url, '_blank');  // Opens the link in a new tab
+  }
+  mainPicture.onclick = () => {
+    linkToNewTab(link)
+  }
+
   
+
+
 
 }
 

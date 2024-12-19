d58dc11c1a46561560be3447e65b124e2b7ef7b7
Brian Ali <gamerfunky78@gmail.com>
2024-12-19 18:25:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ async function getNews() {
 
 
 
-  linkForPic1 = data.stories[11].slug
-  linkForPic2 = data.stories[19].slug
+  linkForPic1 = data.stories[10].slug
+  linkForPic2 = data.stories[18].slug
 
   link1.href = `https://www.google.com/search?q=${encodeURIComponent(linkForPic1)}`;
   link2.href = `https://www.google.com/search?q=${encodeURIComponent(linkForPic2)}`;
@@ -35,8 +35,8 @@ async function getNews() {
     window.open(url, '_blank');  // Opens the link in a new tab
   }
 
-  pic2.src = data.stories[11].content.thumbnail.filename
-  pic3.src = data.stories[19].content.thumbnail.filename
+  pic2.src = data.stories[10].content.thumbnail.filename
+  pic3.src = data.stories[18].content.thumbnail.filename
 
   pic2Desc.textContent = data.stories[1].content.headline
   pic3Desc.textContent = data.stories[2].content.headline
